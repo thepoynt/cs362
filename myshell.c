10ce63506a92fd11ce9f2463246ceaa8a6fb451d
thepoynt <christian.poynter@icloud.com>
2013-09-23 12:16:26 -0500

    working on pipes
    Just to commit what I have

--- myshell.c
+++ myshell.c
@@ -60,9 +60,10 @@ main() {
       continue;
 
     // Handle piping
-    if (is_pipe(args)) {
+    int pipe = is_pipe(args);
+    if (pipe) {
       // call recursive pipe function
-        do_pipe(args);
+        fork_pipes(pipe+1, args);
     } else {
 
       // Check for an ampersand
@@ -104,7 +105,8 @@ main() {
       // Do the command
         do_command(args, block, 
             input, input_filename, 
-            output, output_filename);
+            output, output_filename,
+            0,0,0);
     }
 
     
@@ -146,72 +148,162 @@ int internal_command(char **args) {
 
 /* 
  * Check for a pipe character
+ * return number of pipes
  */
 int is_pipe(char **args) {
   int i;
+  int num_pipes = 0; // number of pipe characters, one less than number of commands
   for(i = 0; args[i] != NULL; i++) {
     if(args[i][0] == '|') {
-      return 1;
+      num_pipes = num_pipes+1;
     }
   }
-  return 0;
+  printf("Number of pipes: %d\n", num_pipes);
+  return num_pipes;
 }
 
 /*
  * get first command from args with pipe, and cut off the pipe
+ * to_exec points to remaining commands
  */
-char **get_pipe_command(char **args) {
+char **get_pipe_command(char **args, char**to_exec) {
   int i;
   char **first_args = EZMALLOC(char *, 20); // allocate char** for first command and it's args
   for(i=0; args[i][0] != '|'; i++) {
     first_args[i] = args[i];
-    
+    to_exec = args[i+1];
   }
 
   return first_args;
 }
 
- /*
-  * recursive method to handle strings of piped commands
-  */
-int do_pipe(char **args) {
+// int spawn_proc (int in, int out, char **args) {
+//   pid_t pid;
+
+//   if ((pid = fork ()) == 0) {
+//     if (in != 0){
+//       dup2 (in, 0);
+//       close (in);
+//     }
+
+//     if (out != 1) {
+//       dup2 (out, 1);
+//       close (out);
+//     }
+
+//     return execvp (args[0], args);
+//   }
+
+//   return pid;
+// }
+
+int fork_pipes (int n, char **args) { // Method based on code from http://stackoverflow.com/questions/8082932/connecting-n-commands-with-pipes-in-a-shell
+  int i;
+  pid_t pid;
+  int in, fd[2];
+  char **to_exec;
+
+  int block; // if the command does not end in an '&'
   int output; // whether output should be redirected
   int input; // whether there is redirected input
   char *output_filename; // name of file for output redirection
   char *input_filename; // name of input file
 
+  /* The first process should get its input from the original file descriptor 0.  */
+  in = 0;
 
-  // grab what's before the first pipe
-  char **first_args = get_pipe_command(args);
-  printf("doing pipe with: ");
-  print_args(first_args);
+  /* Note the loop bound, we spawn here all, but the last stage of the pipeline.  */
+  for (i = 0; i < n - 1; ++i)
+    {
+      pipe (fd);
 
-  // Check for redirected input
-  input = redirect_input(first_args, &input_filename);
+      /* f[1] is the write end of the pipe, we carry `in` from the prev iteration.  */
+      char **first_args = get_pipe_command(args, to_exec);
 
-  // Check for redirected output
-  output = redirect_output(first_args, &output_filename);
+      // Check for an ampersand
+      block = (ampersand(first_args) == 0); // block if not a background process
 
-  // run that command
-  do_command(first_args, block, 
-          input, input_filename, 
-          output, output_filename);
-  // if there's another command
-    // put this command's output in stdin
+      // Check for redirected input
+      input = redirect_input(first_args, &input_filename);
 
-    // recursively call this method with rest of args
+      // Check for redirected output
+      output = redirect_output(first_args, &output_filename);
 
-  // else, put this commands's output in stdout
+      // spawn_proc (in, fd[1], first_args);
+      do_command(first_args, block, 
+                input, input_filename, 
+                output, output_filename,
+                in, fd[1], 1);
 
-  return 0;
+      /* No need for the write and of the pipe, the child will write here.  */
+      close (fd [1]);
+
+      /* Keep the read end of the pipe, the next child will read from there.  */
+      in = fd [0];
+    }
+
+  /* Last stage of the pipeline - set stdin be the read end of the previous pipe
+     and output to the original file descriptor 1. */  
+  if (in != 0)
+    dup2 (in, 0);
+
+  /* Execute the last stage with the current process. */
+  return execvp (cmd [i].argv [0], (char * const *)cmd [i].argv);
 }
+
+// /*
+//   * method to handle strings of piped commands, calls do_pip_recursive
+//   */
+// int do_pipe(char **args) {
+
+// }
+
+//  /*
+//   * recursive method to handle strings of piped commands
+//   */
+// int do_pipe_recursive(char **args, ) {
+//   int output; // whether output should be redirected
+//   int input; // whether there is redirected input
+//   char *output_filename; // name of file for output redirection
+//   char *input_filename; // name of input file
+
+//   if(is_pipe(args)) {
+//     // grab what's before the first pipe
+//     char **first_args = get_pipe_command(args);
+//     printf("doing pipe with: ");
+//     print_args(first_args);
+
+//     // Check for redirected input
+//     input = redirect_input(first_args, &input_filename);
+
+//     // Check for redirected output
+//     output = redirect_output(first_args, &output_filename);
+
+//     // redirect stdout to pipe
+
+//     // run that command
+//     do_command(first_args, block, 
+//             input, input_filename, 
+//             output, output_filename);
+    
+//     // recursively call this method with rest of args
+
+
+//   // if there's another command
+//   } else {
+//     // else, put this commands's output in stdout
+
+//   }
+//   return 0;
+// }
 
 /* 
  * Do the command
  */
 int do_command(char **args, int block,
 	       int input, char *input_filename,
-	       int output, char *output_filename) {
+	       int output, char *output_filename,
+         int in, int out, int is_pipe) {
   
   int result;
   pid_t child_id;
@@ -232,45 +324,58 @@ int do_command(char **args, int block,
 
   if(child_id == 0) {
 
-    // Set up stuff for the command
-
-    // file redirection both in and out
-    printf("output:%d\n", output);
-    print_args(args);
-    if (input && (output >= 1)) { 
-      freopen(input_filename, "r", stdin);
-      if(output == 1) { // re-write file, or create
-        printf("input and output\n");
-        freopen(output_filename, "w+", stdout);
-      } else { // append file, or create
-        printf("input and output append\n");
-        freopen(output_filename, "a", stdout);
+    // If we're coming from a pipe, handle I/O from in and out
+    if(is_pipe) {
+      if (in != 0){
+        dup2 (in, 0);
+        close (in);
       }
-    }
-    // command with file redirection coming in
-    else if (input) { 
-      printf("input\n");
-      freopen(input_filename, "r", stdin);
-    }
-    // command with output redirected to file
-    else if (output >= 1) { 
-      if(output == 1) { // re-write file, or create
-        printf("output\n");
-        freopen(output_filename, "w+", stdout);
-      } else { // append file, or create
-        printf("output append\n");
-        freopen(output_filename, "a", stdout);
-      }
-    }
-    // command with no arguments
-    else if (args[1] == NULL) { 
-      printf("no args\n");
-    } 
-    // command with arguments
-    else {
-      printf("args\n");
-    }
 
+      if (out != 1) {
+        dup2 (out, 1);
+        close (out);
+      }
+    // Otherwise, handle normally
+    } else {
+
+      // file redirection both in and out
+      printf("output:%d\n", output);
+      print_args(args);
+      if (input && (output >= 1)) { 
+        freopen(input_filename, "r", stdin);
+        if(output == 1) { // re-write file, or create
+          printf("input and output\n");
+          freopen(output_filename, "w+", stdout);
+        } else { // append file, or create
+          printf("input and output append\n");
+          freopen(output_filename, "a", stdout);
+        }
+      }
+      // command with file redirection coming in
+      else if (input) { 
+        printf("input\n");
+        freopen(input_filename, "r", stdin);
+      }
+      // command with output redirected to file
+      else if (output >= 1) { 
+        if(output == 1) { // re-write file, or create
+          printf("output\n");
+          freopen(output_filename, "w+", stdout);
+        } else { // append file, or create
+          printf("output append\n");
+          freopen(output_filename, "a", stdout);
+        }
+      }
+      // command with no arguments
+      else if (args[1] == NULL) { 
+        printf("no args\n");
+      } 
+      // command with arguments
+      else {
+        printf("args\n");
+      }
+
+    }
     // execute the actual command
     execvp(args[0], args);
 
