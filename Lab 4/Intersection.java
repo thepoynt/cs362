46c29ef2f2b89a086fc1116e3591a703f027e38d
frederja <frederja@uwec.edu>
2013-12-09 03:30:42 -0600

    Finished!
    works :D

--- Lab 4/Intersection.java
+++ Lab 4/Intersection.java
@@ -1,168 +1,108 @@
 package phore;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 
-public class Intersection implements Runnable{
+public class Intersection2 {
 	
-	static ArrayList<Queue<Thread>> streets = new ArrayList<Queue<Thread>>();
 	static ArrayList<Thread> allThreads = new ArrayList<Thread>();
 	static int clk = 1;
+	public static int getClk() {
+		return clk;
+	}
+
 	static int numCars = 0;
 	static boolean empty = true;
-	static long state = 1;
 	static int carsRan = 0; 
 	static int carsMade = 0;
 	static int carsToRun = 0;
-
+	static int carId = 0;
+	
 	static Random randomGen = new Random();
 	
-	static Semaphore carCanGo = new Semaphore(0);
-	static Semaphore clkUp = new Semaphore(1);
+	static volatile Semaphore carCanGo;
+	static volatile Semaphore checkForFinished;
 	
 	public static void main(String[] args) {
-		
-		for(int i = 0; i < 4; i++){
-			Queue<Thread> newStreet = new LinkedList<Thread>();
-			streets.add(newStreet);
-		}
-//		Scanner user_input = new Scanner( System.in );
-//		String first_name;
-//		System.out.print("Enter number of cars you want to run (under 100 for stability reasons): ");
-//		first_name = user_input.next( );
-//		user_input.close();
-//		numCars = Integer.parseInt(first_name);
-		numCars = 50;
-		
-		if(numCars > 100){
-			numCars = 100;
-		}
 
-		while(carsRan  < numCars && clk < 30){
+		carCanGo = new Semaphore(1);
+		checkForFinished = new Semaphore(0);
+
+		
+		Scanner user_input = new Scanner( System.in );
+		String first_name;
+		System.out.print("Enter number of cars you want to run (under 100 for stability reasons): ");
+		first_name = user_input.next( );
+		user_input.close();
+		numCars = Integer.parseInt(first_name);
+		
+		long startTime = System.currentTimeMillis();
+
+		while(carsRan<numCars){
 			if(carsMade < numCars){
 				newCarsArrive();
-				if(carsToRun == 0){
-					clkUp.release();
-				}
 			}
 			
 			try {
-				System.out.println("clockups");
-				
-				clkUp.acquire();
-				clk++;
-				carCanGo.release();
-				System.out.println("clk: " + clk);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+				checkForFinished.acquire();
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
 			}
-			
-			//check for dead processes
 			for(int i = 0; i < allThreads.size(); i++){
 				if(!(allThreads.get(i).isAlive())){
 					try {
 						Thread t = allThreads.get(i);
-						System.out.println("--Thread " + t.getId() + " Done--");
 						allThreads.remove(i);
 						t.join();
-//						i = allThreads.size();
+						i = allThreads.size();
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
-				}else{
-					System.out.println("~~Thread " + allThreads.get(i).getId() + " Not Done~~");
 				}
 			}
+			carCanGo.release();
 		}
+		
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		System.out.println("Ran in " + totalTime + " ms");
 	}
 	
-	private static int newCarsArrive() {
-		int newcars = 0;
-		int randInt = randomGen.nextInt(10);
-		for(int i = 0; (i <  randInt) && (carsMade <= numCars) && (carsToRun < 100); i++){
-			Thread t = new Thread();
+	private static void newCarsArrive() {
+		int randInt = randomGen.nextInt(5) + 1;
+		for(int i = 0; (i < randInt) && (carsMade <= numCars) && (carsToRun < 100); i++){
+			Thread t = new Thread(new Car(carCanGo, checkForFinished, carId));
+			carId++;
 			allThreads.add(t);
-			newcars++;
 			carsToRun++;
 			carsMade++;
-			System.out.println("made new car " + t.getId() + " | cars made: " + carsMade + " | carsToRun: " + carsToRun);
 			t.start();
 		}
-		return newcars;
-	}
-
-	static double rndom() { 
-		long A = 48271; 
-		long M = 2147483647; 
-		long Q = M/A; 
-		long R = M%A; 
-		 
-		state = 1; 
-		long t = A * (state % Q) - R * (state / Q); 
-		 
-		if (t > 0) 
-			state = t;
-		else 
-			state = t + M;
-		return ((double) state/M); 
-	} 
-	
-	public void run() {
-		System.out.println("here");
-		//aka, this is the arrive function.
-		Thread t = Thread.currentThread();
-		if (empty) { 
-			System.out.println("empty means go");
-			empty = false; 
-			try {
-				drive(0, t);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} 
-		}else { 
-			Random randomGen = new Random();
-			int randomNum = randomGen.nextInt(100);
-			int streetNum = randomNum;
-			streets.get(streetNum).add(t);
-			System.out.println(t.getId() + " | " + streetNum + " | " + randomNum);
-			
-			// wait for someone to signal that it is street[i]’s turn to go 
-			boolean spin = true;
-			while(spin){
-				if(t==streets.get(streetNum).peek()){
-					spin = false;
-				}
-			}
-
-			try {
-				carCanGo.acquire();
-				drive(streetNum, t);
-				clkUp.release();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		} 
 	}
 	
-	void drive(int street, Thread t) throws InterruptedException{
-		//critical section
-		Thread.sleep(10);
-		
-		//clk++;
-		
-		//poll() removes from a Queue
-		streets.get(street).poll();
-		
-		if (streets.get(0).isEmpty() && streets.get(1).isEmpty() && streets.get(2).isEmpty() && streets.get(3).isEmpty()) {
-		      empty = true;
-		}
-		
-		System.out.println("Car #" + t.getId() + " crossed street[" + street + "] at clk: " + clk);
-		carsRan++;
-		carsToRun--;
+	public static void changeStats(int a, int b, int c){
+		carsRan = carsRan + a;
+		carsToRun = carsToRun + b;
+		clk = clk + c;
 	}
+	
+	//was going to use this code, but it doesn't work in Java, so i used the built-in random functions.
+//	static double rndom() { 
+//		long A = 48271; 
+//		long M = 2147483647; 
+//		long Q = M/A; 
+//		long R = M%A; 
+//		 
+//		state = 1; 
+//		long t = A * (state % Q) - R * (state / Q); 
+//		 
+//		if (t > 0) 
+//			state = t;
+//		else 
+//			state = t + M;
+//		return ((double) state/M); 
+//	} 
+
 }
